1238e5500bed91f5e36bc88b818c33b3c46379e5
The Ryoka <sairenyaa@gmail.com>
2026-01-08 10:44:41 +0100

    Adds click sound effect to button
    Improves user experience by adding a sound effect when the click button is pressed.
    
    Also, wraps the heading in a section.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const button = document.getElementById('click-button');
 const counterDisplay = document.getElementById('click-counter');
 
+const clickSound = new Audio("sounds/click.mp3");
+
 let clickCount = 0;
 
 button.addEventListener('click', () => {
     clickCount++;
     counterDisplay.textContent = `Clicks: ${clickCount}`;
+    clickSound.play();
 })
